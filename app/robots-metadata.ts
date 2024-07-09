6f511ae91deefe6dc9c596030c810b0226eaa075
johnlomat <edwardlomat1503@gmail.com>
2024-07-09 19:22:48 +0800

    Created robots metadata

---
+++ app/robots-metadata.ts
@@ -0,0 +1,18 @@
+import { Metadata } from "next";
+
+export const robots: Metadata = {
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+    googleBot: {
+      index: false,
+      follow: false,
+      noimageindex: true,
+      noarchive: true,
+      nositelinkssearchbox: true,
+      nosnippet: true,
+      notranslate: true,
+    },
+  },
+};
