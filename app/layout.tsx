bb0fc7a315952f2b66c0bea23207076b6077ba0b
johnlomat <edwardlomat1503@gmail.com>
2024-07-09 18:55:55 +0800

    Added no index rule for robots

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Libre_Franklin } from "next/font/google";
+import { Metadata } from "next";
 import "./globals.css";
 
 const libre_franklin = Libre_Franklin({
@@ -6,6 +7,23 @@ const libre_franklin = Libre_Franklin({
   subsets: ["latin"],
   variable: "--font-libre-franklin",
 });
+
+export const metadata: Metadata = {
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+    googleBot: {
+      index: false,
+      follow: false,
+      noimageindex: true,
+      noarchive: true,
+      nositelinkssearchbox: true,
+      nosnippet: true,
+      notranslate: true,
+    },
+  },
+};
 
 export default function RootLayout({
   children,
