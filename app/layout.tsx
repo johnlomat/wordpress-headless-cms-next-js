f606cf273895f874989a67b42f3a82e88cb148e3
johnlomat <edwardlomat1503@gmail.com>
2024-07-12 20:13:39 +0800

    Added nextjs-toploader

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Libre_Franklin } from "next/font/google";
+import NextTopLoader from "nextjs-toploader";
 
 import "./globals.css";
 
@@ -15,7 +16,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={libre_franklin.variable}>{children}</body>
+      <body className={libre_franklin.variable}>
+        <NextTopLoader
+          color="#61BD93"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3}
+          crawl={true}
+          showSpinner={true}
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+        />
+        {children}
+      </body>
     </html>
   );
 }
